a4d1637883bb396ccf3596a5165bca4fa8f0ee7f
Sagar3195 <saggyd95@gmail.com>
2023-06-06 10:22:54 +0530

    Data Ingestion done

---
+++ src/pipeline/train_pipeline.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from src.logger import logging
+from src.exception import CustomException
+
+from src.components.data_ingestion import DataIngestion
+
+
+
+if __name__=='__main__':
+    obj= DataIngestion()
+    obj.initiate_data_ingestion()
+
+    
